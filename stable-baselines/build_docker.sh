cdcad3f0281095362efbb08b876a5d9993ffcd80
Kenneth Tang <32535494+ChengYen-Tang@users.noreply.github.com>
2019-11-24 21:53:30 +0800

    add stable-baselines script

---
+++ stable-baselines/build_docker.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+CPU_PARENT=ubuntu:16.04
+GPU_PARENT=nvidia/cuda:9.0-cudnn7-runtime-ubuntu16.04
+
+TAG=ntutselab/stable-baselines
+VERSION=v2.9.0
+
+if [[ ${USE_GPU} == "True" ]]; then
+  PARENT=${GPU_PARENT}
+  TAG="${TAG}-gpu"
+else
+  PARENT=${CPU_PARENT}
+  TAG="${TAG}-cpu"
+fi
+
+if [[ ${INSTALL_MPI} == "True" ]]; then
+  VERSION="${VERSION}-MPI"
+  docker build --build-arg PARENT_IMAGE=${PARENT} --build-arg INSTALL_MPI=${INSTALL_MPI} --build-arg USE_GPU=${USE_GPU}  -t ${TAG}:${VERSION} .
+else
+  docker build --build-arg PARENT_IMAGE=${PARENT} --build-arg INSTALL_MPI=${INSTALL_MPI} --build-arg USE_GPU=${USE_GPU} -t ${TAG}:${VERSION} -t ${TAG} .
