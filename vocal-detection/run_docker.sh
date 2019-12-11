e8dd411aed4d171e7a00d81a494be31aacdefdc8
linitachi <linitachi@gmail.com>
2019-12-11 14:17:16 +0800

    update run_docker

--- vocal-detection/run_docker.sh
+++ vocal-detection/run_docker.sh
@@ -6,7 +6,11 @@ cmd_line="$@"
 echo "Executing in the docker (gpu image):"
 echo $cmd_line
 
-docker run --gpus all -it --rm --network host --ipc=host \
+if [[ -z ${GPUS} ]]; then
+    GPUS='all'
+fi
+
+docker run --gpus ${GPUS} -it --rm --network host --ipc=host \
  --mount src=$(pwd),target=/root/code/,type=bind \
  ntutselab/vocal-detection:latest \
  bash -c "cd /root/code/ && $cmd_line"
