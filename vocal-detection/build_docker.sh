a8f7570c37c4c9e8d809cc18097fe74518c92f9f
linitachi <linitachi@gmail.com>
2019-12-19 19:50:11 +0800

    revise librosa error

--- vocal-detection/build_docker.sh
+++ vocal-detection/build_docker.sh
@@ -2,6 +2,6 @@
 PARENT=nvidia/cuda:10.0-cudnn7-runtime-ubuntu16.04
 
 TAG=ntutselab/vocal-detection
-VERSION=v2.0.0
+VERSION=v2.0.1
 
 docker build --build-arg PARENT_IMAGE=${PARENT} -t ${TAG}:${VERSION} -t ${TAG} .
